5b557d51743202d50332a3fd147e14bcdf35e2f2
Jake Goldsborough <jake.goldsborough@pm.me>
2024-09-28 02:46:39 +0000

    updating wezterm colors

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,6 +1,8 @@
 local wezterm = require 'wezterm'
 local config = {}
 
+local purple = '#b16286'
+
 config.color_scheme = 'Gruvbox dark, hard (base16)'
 config.font = wezterm.font 'Berkeley Mono'
 
@@ -13,11 +15,11 @@ config.hide_tab_bar_if_only_one_tab = false
 config.use_fancy_tab_bar = false
 config.colors = {
   tab_bar = {
-    background = '#98971a',
+    background = purple,
 
     active_tab = {
       bg_color = '#3c3836',
-      fg_color = '#fe8019',
+      fg_color = purple,
     },
   },
 }
