6b0750efcf1ea448c519ea60aa88cf78e2304f3b
Jake Goldsborough <jake.goldsborough@pm.me>
2025-07-03 23:07:19 +0000

    enabling flakes

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -242,6 +242,11 @@ in
     };
   };
 
+  nix.settings.experimental-features = [
+    "nix-command"
+    "flakes"
+  ];
+
   # Copy the NixOS configuration file and link it from the resulting system
   # (/run/current-system/configuration.nix). This is useful in case you
   # accidentally delete configuration.nix.
