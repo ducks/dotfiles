21ac175a385955dcffcf5c493e6da94578af3004
Jake Goldsborough <jake.goldsborough@pm.me>
2025-07-22 13:55:06 -0700

    updating kolide module

--- nixos/modules/kolide.nix
+++ nixos/modules/kolide.nix
@@ -3,18 +3,18 @@
 let
   nix-agent-flake = builtins.fetchTarball {
     url = "https://github.com/kolide/nix-agent/archive/refs/heads/main.tar.gz";
-    sha256 = "<FILL_ME_IN>";
+    sha256 = "0jda5ic2pbb852aj5pqq4a8c06qbgqm1mhdam1w3h0jvljyjjihp";
   };
 in
 {
   imports = [
-    "${nix-agent-flake}/nixos"
+    "${nix-agent-flake}/modules/kolide-launcher"
   ];
 
-  services.nix-agent = {
+  services.kolide-launcher = {
     enable = true;
-    enrollSecretPath = "/etc/kolide/secret";
-    kolideHostname = "your-team.kolide.com";
+    kolideHostname = "k2.kolide.com";
+    updateChannel = "nightly";
   };
 }
 
